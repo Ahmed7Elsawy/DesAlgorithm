936c41d62d0c15e9c778211b7e94ac98fbe89bf6
sql_ask_project <ahmedelsawy487@gmail.com>
2019-11-16 03:59:53 +0200

    Initial commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("DES Cipher");
+        primaryStage.setTitle("DES Algorithm");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
